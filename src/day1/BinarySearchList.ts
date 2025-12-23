bf8f5ea292375534ecda1ffc12d45c033d54393a
matteo-brandolino <matteo.brandolino@gmail.com>
2025-12-23 12:30:54 +0100

    Implement binary search and two crystal balls algorithms

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        // 1. find middle point value
+        const mid = Math.floor((lo + hi) / 2); // [0.....m.....hi] or // low + (hi-lo)/2 avoid integer in c/c++/java
+        const midVal = haystack[mid];
+        // 2. midVal is needle
+        if (needle === midVal) {
+            return true;
+        } else if (needle > midVal) {
+            // 3. needle > midVal [0....m...n..hi]
+            // search only right side
+            lo = mid + 1;
+        } else {
+            //or needle MidVal [0....n...m.....hi]
+            hi = mid;
+        }
+    } while (lo < hi);
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,28 @@
+// [f,f,f,f,t,t,t]
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmount;
 
+    // 1. Jump by √N until first ball breaks
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    /*  Indice:  0  1  2  3  4  5  6  7  8  9
+        Array:  [f, f, f, f, t, t, t, t, t, t]
+                       ^     ^
+                        2    4 <- first break
+                        └─ back to here! */
+
+    // 2. Go back one jump
+    i -= jumpAmount;
+
+    // Step 3: Walk linearly with second ball (max √N steps)
+    for (let j = 0; j <= jumpAmount && i < breaks.length; j++, i++) {
+        if (breaks[i]) return i;
+    }
+
+    return -1;
 }
