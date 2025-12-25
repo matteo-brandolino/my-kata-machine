e1e0579094ef42997ded80c32c06c316e1fb4714
matteo-brandolino <matteo.brandolino@gmail.com>
2025-12-25 14:51:34 +0100

    implement bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,25 @@
-export default function bubble_sort(arr: number[]): void {
+/*  [1,3,7,6,5]
+1.  [1>3] ok
+    [3>7] ok
+    [7> 6] swap
+    [7> 5] swap
+    [1,3,6,5,|7]
+    7 is the largest for sure
+2.  [1>3] ok
+    [3>6] ok
+    [6>5] swap
+    ....    
+*/
 
+export default function bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                //swap
+                const tmp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
 }
