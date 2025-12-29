8b56f9fc338260fbc41ebff0edaf51a04f981a23
matteo-brandolino <matteo.brandolino@gmail.com>
2025-12-29 18:10:06 +0100

    Implement Queue and Stack data structures

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,45 @@
+// A -> B -> C -> D
+// head           tail
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
-
+    private head?: Node<T>;
+    private tail?: Node<T>;
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
-
-}
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+        this.tail.next = node; // move link 
+        this.tail = node; // and then move the tail
+    }
     deque(): T | undefined {
+        // A -X- B -> C -> D
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+        //free head.next = undefined
 
-}
+        if (!this.head) {
+            this.tail = undefined;
+        }
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
